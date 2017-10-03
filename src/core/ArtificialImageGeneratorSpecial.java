9ef1b963a24583ff6055a097a816baaab3cbf755
akramhar <alex.83@bluewin.ch>
2017-10-03 12:19:00 -0400

    Commented added parts

--- src/core/ArtificialImageGeneratorSpecial.java
+++ src/core/ArtificialImageGeneratorSpecial.java
@@ -13,35 +13,35 @@ import gui.ArtificialImageGeneratorGUI;
 
 /**
  * Image generator engine
- * 
+ *
  * @author Alex Rigano
  * @version 0.4, Beta
  */
 public class ArtificialImageGeneratorSpecial extends
-        ArtificialImageGeneratorStandard {
+		ArtificialImageGeneratorStandard {
 
 	private final Random rand;
 
 	public ArtificialImageGeneratorSpecial(final File folder,
-	        final int numOfDatasets, final String imageName,
-	        final int imagePostfixDigits, final int numOfFrames,
-	        final int height, final int width, final Bit bits,
-	        final Color colors, final Double backgroundValue,
-	        final int numOfParticles, final int particlesDist,
-	        final List<Double> signalPeakValues,
-	        final MovementDirection movDirection, final Double movSpeed,
-	        final int radius, final int sigmaValue,
-	        final boolean hasBackgroundGen, final boolean hasParticleGen,
-	        final boolean hasGaussianGen, final boolean hasPoissonGen,
-	        final boolean hasParticleLogGen,
-	        final boolean hasDoubleValueLogGen,
-	        final ArtificialImageGeneratorGUI gui) {
+			final int numOfDatasets, final String imageName,
+			final int imagePostfixDigits, final int numOfFrames,
+			final int height, final int width, final Bit bits,
+			final Color colors, final Double backgroundValue,
+			final int numOfParticles, final int particlesDist,
+			final List<Double> signalPeakValues,
+			final MovementDirection movDirection, final Double movSpeed,
+			final int radius, final int sigmaValue,
+			final boolean hasBackgroundGen, final boolean hasParticleGen,
+			final boolean hasGaussianGen, final boolean hasPoissonGen,
+			final boolean hasParticleLogGen,
+			final boolean hasDoubleValueLogGen,
+			final ArtificialImageGeneratorGUI gui) {
 		super(folder, numOfDatasets, imageName, imagePostfixDigits,
-		        numOfFrames, height, width, bits, colors, backgroundValue,
-		        numOfParticles, particlesDist, signalPeakValues, movDirection,
-		        movSpeed, radius, sigmaValue, hasBackgroundGen, hasParticleGen,
-		        hasGaussianGen, hasPoissonGen, hasParticleLogGen,
-		        hasDoubleValueLogGen, gui);
+				numOfFrames, height, width, bits, colors, backgroundValue,
+				numOfParticles, particlesDist, signalPeakValues, movDirection,
+				movSpeed, radius, sigmaValue, hasBackgroundGen, hasParticleGen,
+				hasGaussianGen, hasPoissonGen, hasParticleLogGen,
+				hasDoubleValueLogGen, gui);
 		this.rand = new Random(System.nanoTime());
 	}
 
@@ -50,12 +50,12 @@ public class ArtificialImageGeneratorSpecial extends
 	 * Generate the given number of particles (centers only) in all the frames.
 	 * The particles will have the given signal value and will move in the given
 	 * direction at the given speed.
-	 * 
+	 *
 	 * @param numOfParticles
 	 * @param signalPeakValue
 	 * @param movDir
 	 * @param movSpeed
-	 * 
+	 *
 	 * @since 0.1
 	 */
 	@Override
@@ -72,13 +72,13 @@ public class ArtificialImageGeneratorSpecial extends
 		final List<Double[][]> frames = this.getFrames();
 
 		final BigDecimal halfX = new BigDecimal(this.getHeight()).divide(
-		        new BigDecimal(2)).setScale(0, BigDecimal.ROUND_HALF_UP);
+				new BigDecimal(2)).setScale(0, BigDecimal.ROUND_HALF_UP);
 		final BigDecimal halfY = new BigDecimal(this.getWidth()).divide(
-		        new BigDecimal(2)).setScale(0, BigDecimal.ROUND_HALF_UP);
+				new BigDecimal(2)).setScale(0, BigDecimal.ROUND_HALF_UP);
 		final BigDecimal startingPositionX = new BigDecimal(halfX.intValue()
-		        - ctr);
+				- (ctr));
 		final BigDecimal startingPositionY = new BigDecimal(halfY.intValue()
-		        - ctr);
+				- (ctr));
 
 		for (int frameIndex = 0; frameIndex < frames.size(); frameIndex++) {
 			final List<MyPoint> frameParticles = new ArrayList<MyPoint>();
@@ -89,18 +89,24 @@ public class ArtificialImageGeneratorSpecial extends
 				final Double randomPartY = this.rand.nextDouble();
 
 				final BigDecimal x = startingPositionX.add(new BigDecimal(
-				        randomPartX));
+						randomPartX));
 				final BigDecimal y = startingPositionY.add(new BigDecimal(
-				        randomPartY));
+						randomPartY));
 
 				final MyPoint newParticle = new MyPoint(x.doubleValue(),
-				        y.doubleValue());
+						y.doubleValue());
 
 				final MyPoint particleCopy = new MyPoint();
-				particleCopy.xD = newParticle.xD.subtract(new BigDecimal(1.50))
-				        .add(new BigDecimal(this.getCTR()));
-				particleCopy.yD = newParticle.yD.subtract(new BigDecimal(1.50))
-				        .add(new BigDecimal(this.getCTR()));
+				particleCopy.xD = newParticle.xD;
+				// particleCopy.xD = newParticle.xD.subtract(new
+				// BigDecimal(1.50));
+				// particleCopy.xD = particleCopy.xD.add(new BigDecimal(this
+				// .getCTR()));
+				particleCopy.yD = newParticle.yD;
+				// particleCopy.yD = newParticle.yD.subtract(new
+				// BigDecimal(1.50));
+				// particleCopy.yD = particleCopy.yD.add(new BigDecimal(this
+				// .getCTR()));
 				particleCopy.computeIntegerValues();
 
 				// firstFrame.put(this.computePoint(actualPoint.x,
